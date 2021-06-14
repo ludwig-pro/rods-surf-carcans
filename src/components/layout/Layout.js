14850e52e05b14411b789f9639d703a90cb4abf3
ludwig-pro <hello@ludwigvantours.dev>
2021-06-14 21:50:37 +0200

    landing

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import { Helmet } from 'react-helmet';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Helmet>
       <Header />
       <main className="text-gray-900">{children}</main>
       <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: {
           lighter: 'hsl(207, 73%, 52%)',
           default: 'hsl(207, 73%, 57%)',
-          darker: 'hsl(207, 73%, 44%)'
-        }
-      }
-    }
+          darker: 'hsl(207, 73%, 44%)',
+        },
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [require('@tailwindcss/forms')],
 };
