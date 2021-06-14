2282057e2964e9d58a2d688eadce532af2ca47f3
ludwig-pro <hello@ludwigvantours.dev>
2021-06-14 22:15:13 +0200

    menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -339,12 +339,10 @@ export default function Example() {
                   <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                     <div className="pt-5 pb-6 px-5">
                       <div className="flex items-center justify-between">
-                        <div>
-                          <img
-                            className="h-8 w-auto"
-                            src="https://tailwindui.com/img/logos/workflow-mark-purple-600-to-indigo-600.svg"
-                            alt="Workflow"
-                          />
+                        <div className="h-8 w-auto">
+                          <span className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                            {`<logo rod's surf>`}
+                          </span>
                         </div>
                         <div className="-mr-2">
                           <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -378,34 +376,20 @@ export default function Example() {
                           href="#"
                           className="text-base font-medium text-gray-900 hover:text-gray-700"
                         >
-                          Pricing
+                          Shape
                         </a>
                         <a
                           href="#"
                           className="text-base font-medium text-gray-900 hover:text-gray-700"
                         >
-                          Partners
+                          School
                         </a>
                         <a
                           href="#"
                           className="text-base font-medium text-gray-900 hover:text-gray-700"
                         >
-                          Company
+                          Shop
                         </a>
-                      </div>
-                      <div className="mt-6">
-                        <a
-                          href="#"
-                          className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700"
-                        >
-                          Sign up
-                        </a>
-                        <p className="mt-6 text-center text-base font-medium text-gray-500">
-                          Existing customer?
-                          <a href="#" className="text-gray-900">
-                            Sign in
-                          </a>
-                        </p>
                       </div>
                     </div>
                   </div>
