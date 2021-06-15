68bd7d644de7121b7dc2f5a764c7a5b380dae453
ludwig-pro <hello@ludwigvantours.dev>
2021-06-15 09:30:15 +0200

    GradiantButton

---
+++ src/css/themes.js
@@ -0,0 +1,8 @@
+const colors = {
+  'USAFA Blue': '#175781',
+  'Pacific Blue': '#00a9ca',
+  'Medium Turquoise': '#5bcfc8',
+  'Deep Saffron': '#f59a3f',
+  'Orange Yellow Crayola': '#fad263',
+  'Papaya Whip': '#fbefcc',
+};
